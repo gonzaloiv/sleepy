ec12820bd4548a11bd3e813da98508d57da9f205
gonzaloiv <aelthalion@gmail.com>
2015-07-02 14:48:02 +0200

    Media queries and cleaning

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -7,26 +7,32 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-		
-	<div class="entry-content">
-			<hr>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php the_content(); ?>
-		<?php
+        <div class="entry-content">
+
+            <div class="content">
+
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header>
+                <!-- .entry-header -->
+                <hr>
+                <?php the_content(); ?>
+                    <?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sleepy' ),
 				'after'  => '</div>',
 			) );
 		?>
+            </div>
 
-	</div><!-- .entry-content -->
+        </div>
+        <!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php edit_post_link( esc_html__( 'Edit', 'sleepy' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
-
+        <footer class="entry-footer">
+            <?php edit_post_link( esc_html__( 'Edit', 'sleepy' ), '<span class="edit-link">', '</span>' ); ?>
+        </footer>
+        <!-- .entry-footer -->
+    </article>
+    <!-- #post-## -->
